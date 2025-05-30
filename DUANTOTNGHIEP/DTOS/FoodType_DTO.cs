437b4ec80672c66e3305c9906029c4bc03572824
Thuandeptrai123 <thuancom284@gmail.com>
2025-05-31 02:46:09 +0700

    Feat_Quanlyloaimonan

---
+++ DUANTOTNGHIEP/DTOS/FoodType_DTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DUANTOTNGHIEP.DTOS
+{
+    public class FoodType_DTO
+    {
+        public Guid FoodTypeId { get; set; }
+        public string FoodTypeName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
