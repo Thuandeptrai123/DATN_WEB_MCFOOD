f545e638dc205d2153116c897bbeae56223a9c4b
Thuandeptrai123 <thuancom284@gmail.com>
2025-06-24 15:11:52 +0700

    API Giỏ hàng

---
+++ DUANTOTNGHIEP/Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace DUANTOTNGHIEP.Models
+{
+    public class Cart : BaseModel
+    {
+        public Guid CartId { get; set; }
+        public Guid CustomerId { get; set; }
+        public List<CartItem> Items { get; set; } = new();
+    }
+}
---
+++ DUANTOTNGHIEP/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace DUANTOTNGHIEP.Models
+{
+    public class CartItem
+    {
+        public Guid CartItemId { get; set; }
+        public Guid CartId { get; set; }
+
+        public Guid? FoodId { get; set; }
+        public Guid? ComboId { get; set; }
+        public int Quantity { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
